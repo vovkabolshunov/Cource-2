f5a553993c67758e4130aa45a110fd8b24b77e5a
vovkabolshunov <44864338+vovkabolshunov@users.noreply.github.com>
2020-05-24 22:09:14 +0300

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,17 @@
+  
+CREATE TABLE TotalCases (
+    country VARCHAR(30) NOT NULL PRIMARY KEY
+    ,total_cases INT NOT NULL REFERENCES Rank (total_cases)
+    ,new_cases INT NOT NULL
+    );
+
+CREATE TABLE TotalDeaths (
+    country VARCHAR(30) NOT NULL PRIMARY KEY
+    ,total_deaths INT NOT NULL
+    ,new_deaths INT NOT NULL
+    );
+	
+CREATE TABLE Rank (
+    total_cases INT NOT NULL PRIMARY KEY
+    ,rank INT NOT NULL
+    );
