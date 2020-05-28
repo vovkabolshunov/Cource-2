7ba5e1213aaeca054bc6c4b8efeaf92776cd84c2
vovkabolshunov <44864338+vovkabolshunov@users.noreply.github.com>
2020-05-28 22:35:19 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,16 +1,27 @@
-CREATE TABLE TotalCases (
-    country VARCHAR(30) NOT NULL PRIMARY KEY
-    ,total_cases INT NOT NULL REFERENCES Rank (total_cases)
-    ,new_cases INT NOT NULL
-    );
+CREATE TABLE Cities1(
+    patient_id integer,
+    city_name char(30)
+);
 
-CREATE TABLE TotalDeaths (
-    country VARCHAR(30) NOT NULL PRIMARY KEY
-    ,total_deaths INT NOT NULL
-    ,new_deaths INT NOT NULL
-    );
-	
-CREATE TABLE Rank (
-    total_cases INT NOT NULL PRIMARY KEY
-    ,rank INT NOT NULL
-    );
+ALTER TABLE Cities1
+ADD CONSTRAINT ... PRIMARY KEY (patient_id);
+
+CREATE TABLE States(
+    state_sex char(6)
+);
+
+ALTER TABLE Cities
+ADD CONSTRAINT city_pr PRIMARY KEY (city_name);
+
+CREATE TABLE Cities_Popularity(
+    city_name char(20),
+    patient_id integer,
+    sex_name integer,
+    age_name integer
+);
+
+ALTER TABLE Cities_Popularity
+ADD CONSTRAINT city_fk FOREIGN KEY (city_name) REFERENCES Cities (city_name);
+
+ALTER TABLE Citiesopularity
+ADD CONSTRAINT  patient_fk FOREIGN KEY (patient_id) REFERENCES Cities1 (patient_id);
